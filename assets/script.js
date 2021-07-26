16bd0949b4235aefae7f81d61d8baf26c8668f7d
ErikGIovani <46170949+ErikGIovani@users.noreply.github.com>
2021-07-26 04:00:18 +0000

    Deploying to gh-pages from @ ErikGIovani/url-vacaciones-cody@ee21bfc2b6597ab4b70d5b1ace1be23935910e28 🚀

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,36 @@
-const form = document.querySelector('form');
-let nombre = document.getElementById('nombre');
+const form = document.querySelector("form");
+let nombre = document.getElementById("nombre");
 
-function Borrar(){
-    document.getElementById('vista').classList.remove('vista');
-    document.getElementById('vista').classList.add('hidde');
+function Borrar() {
+  document.getElementById("vista").classList.remove("vista");
+  document.getElementById("vista").classList.add("hidde");
 }
 
-form.addEventListener('submit', e => {
-	e.preventDefault()
-    if (nombre.value == ''){
-        document.getElementById('button-container').classList.add('hidde')
-        document.getElementById('vista').classList.remove('hidde');
-        document.getElementById('vista').classList.add('vista');
-        nombre.value = 'Por Favor Ingrese Un Nombre De Usuario'
-        document.getElementById('link').innerText = (`${nombre.value}`);
-        nombre.value = ''
-        setTimeout(Borrar,3000);
-    }else {
-        document.getElementById('vista').classList.remove('hidde');
-        document.getElementById('button-container').classList.remove('hidde')
-        document.getElementById('vista').classList.add('vista');
-        document.getElementById('link').innerText = (`https://${nombre.value}.github.io/vacaciones-cody`);
-        document.getElementById('button').href =  (`https://${nombre.value}.github.io/vacaciones-cody`);
-        nombre.value = ''
-    };
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (nombre.value == "") {
+    document.getElementById("button-container").classList.add("hidde");
+    document.getElementById("vista").classList.remove("hidde");
+    document.getElementById("vista").classList.add("vista");
+    nombre.value = "Por Favor Ingrese Un Nombre De Usuario";
+    document.getElementById("link").innerText = `${nombre.value}`;
+    nombre.value = "";
+    setTimeout(Borrar, 3000);
+  } else {
+    document.getElementById("vista").classList.remove("hidde");
+    document.getElementById("button-container").classList.remove("hidde");
+    document.getElementById("vista").classList.add("vista");
+    document.getElementById("vista-iframe").classList.remove("hidde");
+    document.getElementById("vista-iframe").classList.add("vista-frame");
+    document.getElementById(
+      "link"
+    ).innerText = `https://${nombre.value}.github.io/vacaciones-cody`;
+    document.getElementById(
+      "button"
+    ).href = `https://${nombre.value}.github.io/vacaciones-cody`;
+    document.getElementById(
+      "iframe"
+    ).src = `https://${nombre.value}.github.io/vacaciones-cody`;
+    nombre.value = "";
+  }
 });
